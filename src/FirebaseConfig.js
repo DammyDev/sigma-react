41ef263d856fd9d0b60c16afe699eab7ea04ef47
ELUDIRE DAMILOLA <40713025+DammyDev@users.noreply.github.com>
2021-04-04 01:26:03 +0100

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -5,7 +5,7 @@ import 'firebase/auth'
  // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyARChRknUCjssKY5IDokGprSbt5BLWOtis",
+    apiKey: "",
     authDomain: "erap-47584.firebaseapp.com",
     databaseURL: "https://erap-47584.firebaseio.com",
     projectId: "erap-47584",
